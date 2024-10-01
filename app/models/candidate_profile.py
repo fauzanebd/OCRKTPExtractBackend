448c08afcc6a44a46e48ec8803bc4df41dfda53e
haritsrizkall <haritsrizkal@mail.ugm.ac.id>
2024-10-01 22:19:51 +0700

    Add image column to CandidateProfile model

--- app/models/candidate_profile.py
+++ app/models/candidate_profile.py
@@ -4,6 +4,7 @@ class CandidateProfile(db.Model):
     __tablename__ = 'candidate_profiles'
     id = db.Column(db.Integer, primary_key=True)
     client_code = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
     text = db.Column(db.Text, nullable=False)
     
     def to_dict(self):
